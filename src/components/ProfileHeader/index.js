bfb84d64ba7ad21c7a04b34a91d958981408fc66
marc <marcliuprofess@gmail.com>
2020-04-16 08:14:31 -0400

    Add Profile Header component

---
+++ src/components/ProfileHeader/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import './style.scss';
+import Avatar from '../../images/img-portrait.png';
+
+const ProfileHeader = ({ data }) => {
+  return (
+    <div className="profile-header">
+      <div className="profile-header-avatar">
+        <img src={Avatar} alt="profile image" />
+      </div>
+      <div className="profile-header-desc">
+        <div className="column-4">
+          <h1 className="profile-header-desc-name">{data.name}</h1>
+          <h2 className="profile-header-desc-location">{data.location}</h2>
+        </div>
+        <div>
+          <h5>{data.age}</h5>
+          <h6>AGE</h6>
+        </div>
+        <div className="column-3">
+          <h5>{data.college}</h5>
+          <h6>COLLEGE</h6>
+        </div>
+        <div>
+          <h5>{data.matchesPlayed}</h5>
+          <h6>MATCHES PLAYED</h6>
+        </div>
+        <div>
+          <h5>{data.index}</h5>
+          <h6>INDEX</h6>
+        </div>
+        <div>
+          <h5>{data.matchesWon}</h5>
+          <h6>MATCHES WON</h6>
+        </div>
+        <div>
+          <h5>{data.matchesLost}</h5>
+          <h6>MATCHES LOST</h6>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileHeader;
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { ProfileHeader } from '../../components'; 
+import './style.scss'
 
 const ProfilePage = () => {
+  const profile = {
+    name: 'Rob Headley',
+    location: 'Black Rock Country Club, Hingam, MA',
+    age: '51',
+    college: 'Virgina Tech',
+    matchesPlayed: '11',
+    index: '7.1',
+    matchesWon: '7',
+    matchesLost: '4'
+  };
+
   return (
-    <div>Profile Page</div>
+    <div className="profile-page">
+      <ProfileHeader data={profile}/>
+    </div>
   )
 }
 
