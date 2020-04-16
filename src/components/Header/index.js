a8141b3349ef1616f203915ae0aa124b9c1187a7
marc <marcliuprofess@gmail.com>
2020-04-16 06:41:29 -0400

    Use enum data on header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,12 +7,12 @@ import './style.scss';
 
 const Header = () => {
   const [item, setItem] = useState(0)
-  const LEFT_MENUS = [INTERNAL_LINKS.CLUBS, INTERNAL_LINKS.MATCHES, INTERNAL_LINKS.PLAYERS, INTERNAL_LINKS.SHOP]
+  const MENUS = [INTERNAL_LINKS.CLUBS, INTERNAL_LINKS.MATCHES, INTERNAL_LINKS.PLAYERS, INTERNAL_LINKS.SHOP]
 
   return (
     <header className="lynksin-header">
       <nav className="navbar navbar-expand-md navbar-light">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href={INTERNAL_LINKS.HOME}>
           <img src={Logo} alt="logo" />
         </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTarget" aria-controls="navbarTarget" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,26 +21,21 @@ const Header = () => {
         <div className="collapse navbar-collapse " id="navbarTarget">
           <Router>
             <ul className="navbar-nav mr-auto">
-              <li className={`nav-item ${item === 0 ? 'active' : ''}`}>
-                <Link className="nav-link" to="/clubs" onClick={() => setItem(0)}>CLUBS </Link>
-              </li>
-              <li className={`nav-item ${item === 1 ? 'active' : ''}`}>
-                <Link className="nav-link" to="/matches" onClick={() => setItem(1)}>MATCHES</Link>
-              </li>
-              <li className={`nav-item ${item === 2 ? 'active' : ''}`}>
-                <Link className="nav-link" to="/players" onClick={() => setItem(2)}>PLAYERS</Link>
-              </li>
-              <li className={`nav-item ${item === 3 ? 'active' : ''}`}>
-                <Link className="nav-link" to="/shop" onClick={() => setItem(3)}>SHOP</Link>
-              </li>
+              {
+                MENUS.map((x, index) => (
+                  <li className={`nav-item ${item === index ? 'active' : ''}`}>
+                    <Link className="nav-link" to={x} onClick={() => setItem(index)}>{x.slice(1).toUpperCase()}</Link>
+                  </li>
+                ))
+              }
             </ul>
             <form className="form-inline my-2 my-lg-0">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to="/login">LOGIN </Link>
+                  <Link className="nav-link" to={INTERNAL_LINKS.LOGIN}>LOGIN </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/register">Sign Up</Link>
+                  <Link className="nav-link" to={INTERNAL_LINKS.REGISTER}>Sign Up</Link>
                 </li>
               </ul>
             </form>
