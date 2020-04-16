68d8f6fd1d1ce083dfcb7e491e8724c8aec9e12d
marc <marcliuprofess@gmail.com>
2020-04-15 21:25:27 -0400

    create the react project

---
+++ src/containers/App/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Hello World!!!
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/enum/InternalLinks.js
@@ -0,0 +1,10 @@
+/**
+ * To store all internal links and their sub-links
+ */
+
+ module.exports = {
+     HOME: '/',
+     NOT_FOUND: '/404',
+     LOGIN: '/login',
+     REGISTER: '/register',
+ }
