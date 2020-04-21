d39d662507a6830a3c4db9451eb9e7488a712787
marcliu <marcliuprofess@gmail.com>
2020-04-21 05:41:54 -0400

    Fix some minor style error

--- src/components/Club/index.js
+++ src/components/Club/index.js
@@ -11,7 +11,10 @@ const Club = ({ className, img, name, location }) => {
       <div className='club-content'>
         <div className='club-content-left'>
           <h5 className='club-content-left-name'>{name}</h5>
-          <h5 className='club-content-left-location'>{location}</h5>
+          <div className="d-flex">
+            <i className="fa fa-map-marker mr-1" aria-hidden="true"></i>
+            <h5 className='club-content-left-location'>{location}</h5>
+          </div>
         </div>
         <div className='club-content-right'>
           <i className="fa fa-heart-o" aria-hidden="true"></i>
